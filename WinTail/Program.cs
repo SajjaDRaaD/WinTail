8db4c8ee7092d98536abe5157eb469bfcaa5138a
SajjaDRaaD <s.ebrahimiraad@KRJ-EBRAHIMI>
2023-06-18 12:17:14 +0330

    Add project files.

---
+++ WinTail/Program.cs
@@ -0,0 +1,14 @@
+﻿using Akka.Actor;
+using WinTail;
+
+
+// Creating actor system object
+var MyActorSystem = ActorSystem.Create("MyActorSystem");
+
+// Creating actors
+var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
+
+consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+MyActorSystem.WhenTerminated.Wait();
